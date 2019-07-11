278f8eb9a71e0f19e2e3714f1e149aafad7ed218
Luis Pedro Coelho <luis@luispedro.org>
2019-07-11 14:03:57 +0200

    ENH Add -F argument. Faster output

--- Data/BioConduit.hs
+++ Data/BioConduit.hs
@@ -8,10 +8,12 @@ module Data.BioConduit
     ) where
 
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Builder as BB
 import qualified Data.Conduit as C
 import           Data.Conduit ((.|))
 import qualified Data.Conduit.Combinators as C
 import qualified Data.Conduit.Binary as C
+import qualified Data.Conduit.List as CL
 import           Data.Word
 import           Control.Monad.IO.Class
 import           Control.Exception
@@ -32,10 +34,10 @@ faseqLength = B.length . seqdata
 greaterThanSign :: Word8
 greaterThanSign = 62
 
-faConduit :: (MonadIO m) => C.Conduit B.ByteString m Fasta
+faConduit :: (MonadIO m) => C.ConduitT B.ByteString Fasta m ()
 faConduit = C.lines .| faConduit'
 
-faConduit' :: (MonadIO m) => C.Conduit B.ByteString m Fasta
+faConduit' :: (MonadIO m) => C.ConduitT B.ByteString Fasta m ()
 faConduit' = C.await >>= \case
                 Nothing -> return ()
                 Just header
@@ -43,7 +45,7 @@ faConduit' = C.await >>= \case
                   | B.head header == greaterThanSign -> getdata 1 (B.drop 1 header) []
                   | otherwise -> liftIO $ throwIO (AssertionFailed "Unexpected data")
   where
-    getdata :: MonadIO m' => Int -> B.ByteString -> [B.ByteString] -> C.Conduit B.ByteString m' Fasta
+    getdata :: MonadIO m' => Int -> B.ByteString -> [B.ByteString] -> C.ConduitT B.ByteString Fasta m' ()
     getdata !n header toks = C.await >>= \case
                                 Nothing -> C.yield $ Fasta header (B.concat $ reverse toks)
                                 Just next
@@ -53,13 +55,8 @@ faConduit' = C.await >>= \case
                                             getdata (n+1) (B.drop 1 next) []
                                     | otherwise -> getdata (n+1) header (next:toks)
 
-faWriteC :: (Monad m) => C.Conduit Fasta m B.ByteString
-faWriteC = C.awaitForever $ \(Fasta h s) -> do
-    C.yield ">"
-    C.yield h
-    C.yield "\n"
-    C.yield s
-    C.yield "\n"
+faWriteC :: (Monad m) => C.ConduitT Fasta BB.Builder m ()
+faWriteC = CL.map $ \(Fasta h s) -> mconcat [BB.char7 '>', BB.byteString h, BB.char7 '\n', BB.byteString s, BB.char7 '\n']
 
-faWriteConduit :: (Monad m) => [Fasta] -> C.Source m B.ByteString
+faWriteConduit :: (Monad m) => [Fasta] -> C.ConduitT () BB.Builder m ()
 faWriteConduit fas = C.yieldMany fas .| faWriteC
--- RetrieveFastaSeqs.hs
+++ RetrieveFastaSeqs.hs
@@ -3,16 +3,17 @@
 {-# LANGUAGE LambdaCase, OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables, FlexibleContexts #-}
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.Conduit.Combinators as CC
 import qualified Data.Conduit.Combinators as C
 import qualified Data.Conduit.List as CL
-import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.Binary as C
 import qualified Data.Conduit as C
 import           Data.Conduit ((.|))
 import qualified Data.Set as S
 import           Data.List (foldl')
 import           System.Console.GetOpt
-import           Control.DeepSeq
+import           System.IO.SafeWrite (withOutputFile)
 import Control.Monad
 import System.Environment
 
@@ -21,19 +22,21 @@ import Data.BioConduit
 
 data CmdArgs = CmdArgs
     { ifilesArg :: [FilePath]
+    , fileListArg :: FilePath
     , keepArg :: FilePath
     , ofileArg :: FilePath
     , verboseArg :: Bool
     } deriving (Show)
 
 parseArgs :: [String] -> CmdArgs
-parseArgs argv = foldl' (flip ($)) (CmdArgs [] "" "" False) flags
+parseArgs argv = foldl' (flip ($)) (CmdArgs [] "" "" "" False) flags
     where
         flags :: [CmdArgs -> CmdArgs]
         (flags, _, _extraOpts) = getOpt Permute options argv
         options =
             [ Option ['o'] ["output"] (ReqArg (\f c -> c { ofileArg = f }) "FILE") "Output file"
             , Option ['k'] ["keep"] (ReqArg (\f c -> c { keepArg = f }) "FILE") "Input file to check"
+            , Option ['F'] ["file-list"] (ReqArg (\f c -> c { fileListArg = f }) "FILE") "List of input files"
             , Option ['i'] ["input"] (ReqArg (\f c -> c { ifilesArg = (f:ifilesArg c) }) "FILE") "Input file to check"
             , Option ['v'] ["verbose"] (NoArg (\c -> c {verboseArg = True }))  "Verbose"
             ]
@@ -49,12 +52,20 @@ readHeaderList cfname =
 main :: IO ()
 main = do
     opts <- parseArgs <$> getArgs
+    fileList <- case (ifilesArg opts, fileListArg opts) of
+        ([], "") -> error "Both -i and -F are empty"
+        ([], fl) -> C.runConduitRes $
+                        C.sourceFile fl .| C.lines .| CL.map B8.unpack .| CL.consume
+        (fs, "") -> return fs
+        (_, _) -> error "Both -i and -F contain values"
     toKeep <- readHeaderList (keepArg opts)
     when (verboseArg opts) $
         putStrLn ("Will keep " ++ show (S.size toKeep) ++ " sequences.")
-    C.runConduitRes $
-        (sequence_ [C.sourceFile f | f <- ifilesArg opts])
-            .| faConduit
-            .| C.filter (flip S.member toKeep . seqheader)
-            .| faWriteC
-            .| CB.sinkFileCautious (ofileArg opts)
+
+    withOutputFile (ofileArg opts) $ \hout ->
+        C.runConduitRes $
+            (sequence_ [C.sourceFile f | f <- fileList])
+                .| faConduit
+                .| C.filter (flip S.member toKeep . seqheader)
+                .| faWriteC
+                .| CC.sinkHandleBuilder hout
